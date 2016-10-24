428d64dbc4478a0fe6540af6de657daff332ee61
Victoria Frantsuh <eboko1@gmail.com>
2016-10-24 20:23:12 +0300

    add GameLauncher

---
+++ src/fvi/at/ua/GameLauncher.java
@@ -0,0 +1,15 @@
+
+package fvi.at.ua;
+
+/**
+ * Created by Vika on 24.10.2016.
+ */
+public class GameLauncher {
+    public static void  main (String[] args){
+        GuessGame game = new GuessGame();
+        game.startGame();
+
+
+    }
+
+}
--- src/fvi/at/ua/GuessGame.java
+++ src/fvi/at/ua/GuessGame.java
@@ -1,5 +1,13 @@
+
+package fvi.at.ua;
+
 /**
  * Created by Vika on 24.10.2016.
  */
 public class GuessGame {
+    public  void startGame(){
+
+
+    }
+
 }
