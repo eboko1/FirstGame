98431978bc47b20ef94a7a12a81d4b784c3c6130
Victoria Frantsuh <eboko1@gmail.com>
2016-10-24 20:28:49 +0300

    add Player

---
+++ src/fvi/at/ua/Player.java
@@ -0,0 +1,13 @@
+
+package fvi.at.ua;
+
+/**
+ * Created by Vika on 24.10.2016.
+ */
+public class Player {
+    int number = 0;
+    public void guess(){
+        number=(int)(Math.random()*10);
+        System.out.print("Думаю, що число "+number);
+    }
+}
